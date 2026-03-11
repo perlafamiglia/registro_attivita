32cb095a389c0cfa25e989fd64b70abc25879b15
georgos9433 <georgos9433@gmail.com>
2026-03-11 17:44:19 +0100

    update table width on bigger screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "8095ceba5d506dfaea0ec727a5749e2d
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "63038b22cd4b045849f97db5e40511f9",
+"flutter_bootstrap.js": "f3e30a458610e48badfad20a3c684b66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b667ff937eccc30d193e81b5a67358eb",
 "/": "b667ff937eccc30d193e81b5a67358eb",
-"main.dart.js": "64c8936a2020e66a091c584c203815d7",
+"main.dart.js": "79f75ffd9ad2e7789e3e678643b54a36",
 "manifest.json": "f8a80fc40dc57f38cdf0ed25fda396a5",
 "version.json": "3eca861091059a0ca1f8caece64fe20c"};
 // The application shell files that are downloaded before a service worker can
